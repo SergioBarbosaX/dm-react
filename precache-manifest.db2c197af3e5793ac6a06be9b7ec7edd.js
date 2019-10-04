de04956797c9456fbc9220c5765f68e2b964790a
SergioBarbosaX <serbarbos@hotmail.com>
2019-10-04 22:05:26 +0200

    Updates

---
+++ precache-manifest.db2c197af3e5793ac6a06be9b7ec7edd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ee73dbf8f6290e8a3838b0d2f71a2eb",
+    "url": "/dm-react/index.html"
+  },
+  {
+    "revision": "6d9f2f2d3f9afc37cfac",
+    "url": "/dm-react/static/css/main.3fb679fb.chunk.css"
+  },
+  {
+    "revision": "7ec637863cecc226b171",
+    "url": "/dm-react/static/js/2.9ba98bc0.chunk.js"
+  },
+  {
+    "revision": "6d9f2f2d3f9afc37cfac",
+    "url": "/dm-react/static/js/main.753a0f5f.chunk.js"
+  },
+  {
+    "revision": "55e65b14e34112da1c0b",
+    "url": "/dm-react/static/js/runtime~main.f95bb820.js"
+  }
+]);
